ae82b226f99d6b44572a948ee0c14fd01df36161
lmachegger <lmachegger@gmail.com>
2022-07-13 22:00:23 +0200

    restructure files

--- src/rate/rate.service.ts
+++ src/rate/rate.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { RateRequestDto, RateResponseDto } from 'src/dtos';
+import { RateRequestDto, RateResponseDto } from './dto';
 
 @Injectable()
 export class RateService {
