d588beee6ca3acd42c9a32564a303369f49e2eb4
lmachegger <lmachegger@gmail.com>
2022-07-14 22:07:04 +0200

    add e2e test

--- test/rate.e2e-spec.ts
+++ test/rate.e2e-spec.ts
@@ -1,11 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
 import { RateRequestDto, RateResponseDto } from 'src/rate/dto';
+import { AppModule } from './../src/app.module';
+import { RateService } from '../src/rate/rate.service';
 
-describe('AppController (e2e)', () => {
+describe('RateController (e2e)', () => {
   let app: INestApplication;
+
+  const defaultRequestBody: RateRequestDto = {
+    rate: {
+      energy: 0.3,
+      time: 2,
+      transaction: 1,
+    },
+    cdr: {
+      meterStart: 1204307,
+      timestampStart: '2021-04-05T10:04:00Z',
+      meterStop: 1215230,
+      timestampStop: '2021-04-05T11:27:00Z',
+    },
+  };
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,37 +28,45 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
     await app.init();
   });
 
-  it('/rate (POST)', () => {
-    const requestBody: RateRequestDto = {
-      rate: {
-        energy: 0.3,
-        time: 2,
-        transaction: 1,
-      },
-      cdr: {
-        meterStart: 1204307,
-        timestampStart: '2021-04-05T10:04:00Z',
-        meterStop: 1215230,
-        timestampStop: '2021-04-05T11:27:00Z',
-      },
-    };
+  describe('/rate (POST)', () => {
+    it('applies rates correctly', () => {
+      const expectedResponse: RateResponseDto = {
+        overall: 7.04,
+        components: {
+          energy: 3.277,
+          time: 2.767,
+          transaction: 1,
+        },
+      };
 
-    const expectedResponse: RateResponseDto = {
-      overall: 7.04,
-      components: {
-        energy: 3.277,
-        time: 2.767,
-        transaction: 1,
-      },
-    };
+      return request(app.getHttpServer())
+        .post('/rate')
+        .send(defaultRequestBody)
+        .expect(200)
+        .expect(expectedResponse);
+    });
 
-    return request(app.getHttpServer())
-      .post('/rate')
-      .send(requestBody)
-      .expect(200)
-      .expect(expectedResponse);
+    it('Request body validation works', () => {
+      const invalidRequestBody: RateRequestDto = {
+        ...defaultRequestBody,
+        cdr: { ...defaultRequestBody.cdr, meterStart: -1 },
+      };
+
+      const expectedResponse = {
+        statusCode: 400,
+        message: ['cdr.meterStart must not be less than 0'],
+        error: 'Bad Request',
+      };
+
+      return request(app.getHttpServer())
+        .post('/rate')
+        .send(invalidRequestBody)
+        .expect(400)
+        .expect(expectedResponse);
+    });
   });
 });
